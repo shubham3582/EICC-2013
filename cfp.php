2649784edea89299012e27ec14f43c289c66634b
Shubham Srivastava <shubham3582@gmail.com>
2013-08-04 13:59:07 +0530

    commit

---
+++ cfp.php
@@ -0,0 +1,95 @@
+<?php 
+session_start();
+
+//check logged in or not!
+if(!isset($_SESSION['email'])){
+
+include('header.php');
+}
+else {
+include('usr/header.php');
+}
+?><head><!-- start: Meta -->
+	<meta charset="utf-8">
+	<title>EICC 2013-Call For Papers</title> 
+	<meta name="description" content="Call For Papers - EICC 2013">
+	<meta name="keywords" content="EICC, EICC 2013, 2nd National Confrence, Galgotia, IT Department, Greater Noida, Confrence">
+	<meta name="author" content="Shubham Srivastava(Final Year Student @ Galgotia">
+	<!-- end: Meta -->
+    </head>
+
+
+<tr>
+			<td>&nbsp;</td>
+			<td valign="top" align="center">
+				<table width="100%" bgcolor="#e7e7e7" style="border-top:1px solid #BBC5C6;border-left:1px solid #BBC5C6;border-right:1px solid #BBC5C6;border-bottom:1px solid #BBC5C6;margin-top:0px;margin-left:0px;">
+					<tbody><tr>
+                    <?php include('menu.php'); ?>
+					</tbody></table>
+				<br>
+				<center></center>
+				<br>
+			</td>
+			<td>&nbsp;</td>
+				
+			<td valign="top" align="center" style="text-align:justify;">
+			<div id="main" class=maincontent style='width:680'>
+<b style="font-size:13pt;font-weight:bold" class=underline02>Call for Papers</b><br><br><br>
+<center>
+<b style="font-size:13pt;font-weight:bold;">EICC 2013: 2<sup>nd</sup> National Conference on Information Technology</b><br><br>
+</center>
+
+
+<p><span style="font-size:11pt;text-align:left;font-weight:bold;color:darkgreen">Important Dates and Paper Types</span>
+  <br><br>
+  <strong>EICC 2013<br>
+    
+    Deadline for Papers: <font color="#666666">15<sup>th</sup> March, 2013</font><br>
+    Paper Acceptance Notification: <font color="#666666">25<sup>th</sup> March, 2013</font>
+  </p>
+  </strong>
+  
+  
+  EICC 2013, 2<sup>nd</sup> National  Conference on Information Technology is going to be held at Galgotias College Of Engineering And Technology, Greater Noida, India.  </p>
+<p>Selected papers will be included as revised monographs in eminent international journals. </p>
+<p><br>
+  <br>
+  
+  <span style="font-size:11pt;text-align:left;font-weight:bold;color:darkgreen">EICC 2013 - Introduction</span><br><br>
+  
+  EICC 2013 stands for 2<sup>nd</sup> National  Conference on Information Technology. It is the premier forum for Theory, Development, Applications, Experiences, and Evaluation of Information Technology. It enables the  international scientific elite, including experienced scientists, promising young researchers and practicians alike  to gather together to discuss their views and research findings.  
+  EICC 2013 promotes fundamental and applied  research continuing advanced academic education and transfers knowledge between involved both sides of and the application of Information Technology.<br><br />
+  This conference will be a platform for distinguished scholars to explore a cutting-edge Information Technology topic. Friendly atmosphere at the conference will promote culture interchange and develop friendship among the partcipants. 
+  <br>
+  <br>
+  
+  <br>
+  <strong>Objectives and Vision</strong>&nbsp;&nbsp;&nbsp;&nbsp;
+  <br><br>
+  The objective of EICC 2013 is to provide a forum for ideas on Information Technology, including advances in information technology, industrial, service, management issues etc.  
+  <br><br>
+  EICC 2013 has brought together software engineers, developers, performance analysts and software/performance modelers who are addressing the challenges of increasing system complexity, rapidly evolving information technologies, short time to market, incomplete documentation, and less-than-adequate methods, models and tools for developing, modeling, and measuring scalable, high-performance applications in Information Technology. The focus of EICC 2013 is therefore in the Multidisciplinary and Hybrid/Convergent research rather than one discipline in isolation.
+  <br><br>
+  It is generally acknowledged that the advancement of technologies has improved the quality of our life and the technologies enabled pervasive access for everyone with ubiquitous devices to ensure a connected global world, global-village. However, current technological solutions, no matter how advanced, by themselves are not enough without taking into account the human beings who actually use them. 
+  <br><br>
+  The organizers of EICC 2013 believe that Multidisciplinary and Hybrid/Convergent research is the most important aspect of success and it will potentially result in even more exciting developments.
+  Interaction Science is one of the primary prospects for novel and future oriented research. We are genuinely aimed at supporting further research and progress of technologies in Information Technology.
+  EICC 2013 welcomes broad genuine ideas aimed at the development of Multidisciplinary and Hybrid/Convergent research in Information Technology. It is our Vision that with the help of this conference we will be able to build sufficient grounds for fulfillment of most brilliant and far-reaching ideas in Information Technology. Based on our previous experiences, we are sure that discussions fueled by this conference will enrich the participants with precious knowledge and experiences. 
+  <br><br>
+  <strong>  Mission and Aims </strong>
+  <br><br>EICC 2013 will trigger off the scientific community to propose topics that should be tackled from research perspective and let the community explain how to best use their tools for practical and theoretical problems of Information Technology. Many various contributions are foreseen from prospective authors. This includes use-cases of theoretical tools and methods to solve practical problems. Such contributions should be as usable as possible by practitioners in the related field. We also expect research results from practitioners that have identified a problem that could be solved by tools from network sciences. One of the missions of EICC 2013 is to make the scientific community aware of the importance of the issues in Information Technology and to suggest means by which the problem may be solved by the scientific community. The contributions should stimulate interaction between theoreticians and practitioners and also have high potential impact in either field.
+  <br><br>
+  We aims to bring together and share brilliant and ideas, accumulated knowledge and unique experiences for mutual benefit of researchers and practitioners and Xplore possible directions for development of Multidisciplinary and Hybrid/Convergent research in the areas of Information Technology. 
+  <br>
+  <br>
+</p>
+
+
+			</div>
+
+			<br>
+			</td>
+			<td>&nbsp;</td>
+		</tr>
+		<?php include('footer.php'); ?>
+
